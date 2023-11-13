4c6015b83ad95246e22843eabff0c10d29c76622
yong <yong@asial.co.jp>
2023-11-13 16:12:58 +0900

    feat: fix css support for Monaca Localkit

--- www/assets/index.js
+++ www/assets/index.js
@@ -303,7 +303,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       </div>
     </div>
   </div>
-`}}Xn.id="6c48e10727";function Gn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
+`}}Xn.id="0d62351434";function Gn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
 <div class="page" data-name="about">
   <div class="navbar">
     <div class="navbar-bg"></div>
@@ -332,7 +332,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
   </div>
 </div>
-`}}Gn.id="1f9dcdbb33";function Un(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
+`}}Gn.id="f3e78d67ff";function Un(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
 <div class="page" data-name="form">
   <div class="navbar">
     <div class="navbar-bg"></div>
@@ -573,7 +573,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
   </div>
 </div>
-`}}Un.id="2bc6a5b525";function Ja(s,{$f7:e,$on:a,$onBeforeMount:t,$onMounted:n,$onBeforeUnmount:r,$onUnmounted:i}){let o="Jimmy",l=25,c=["Tennis","Chess","Football"];const d=()=>{e.dialog.alert("Hello World")};return t(()=>{console.log("onBeforeMount")}),n(()=>{console.log("onMounted")}),r(()=>{console.log("onBeforeUnmount")}),i(()=>{console.log("onUnmounted")}),a("pageMounted",(u,p)=>{console.log("pageMounted",p)}),a("pageInit",(u,p)=>{console.log("pageInit",p)}),a("pageBeforeIn",(u,p)=>{console.log("pageBeforeIn",p)}),a("pageAfterIn",(u,p)=>{console.log("pageAfterIn",p)}),a("pageBeforeOut",(u,p)=>{console.log("pageBeforeOut",p)}),a("pageAfterOut",(u,p)=>{console.log("pageAfterOut",p)}),a("pageBeforeRemove",(u,p)=>{console.log("pageBeforeRemove",p)}),function(u){u.$;var p=u.$h;u.$root,u.$f7;var g=u.$f7route;u.$f7router;var h=u.$theme;return u.$update,u.$store,p`
+`}}Un.id="b783ca45b2";function Ja(s,{$f7:e,$on:a,$onBeforeMount:t,$onMounted:n,$onBeforeUnmount:r,$onUnmounted:i}){let o="Jimmy",l=25,c=["Tennis","Chess","Football"];const d=()=>{e.dialog.alert("Hello World")};return t(()=>{console.log("onBeforeMount")}),n(()=>{console.log("onMounted")}),r(()=>{console.log("onBeforeUnmount")}),i(()=>{console.log("onUnmounted")}),a("pageMounted",(u,p)=>{console.log("pageMounted",p)}),a("pageInit",(u,p)=>{console.log("pageInit",p)}),a("pageBeforeIn",(u,p)=>{console.log("pageBeforeIn",p)}),a("pageAfterIn",(u,p)=>{console.log("pageAfterIn",p)}),a("pageBeforeOut",(u,p)=>{console.log("pageBeforeOut",p)}),a("pageAfterOut",(u,p)=>{console.log("pageAfterOut",p)}),a("pageBeforeRemove",(u,p)=>{console.log("pageBeforeRemove",p)}),function(u){u.$;var p=u.$h;u.$root,u.$f7;var g=u.$f7route;u.$f7router;var h=u.$theme;return u.$update,u.$store,p`
   <div class="page">
     <div class="navbar">
       <div class="navbar-bg"></div>
@@ -629,7 +629,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       </div>
     </div>
   </div>
-`}}Ja.id="fc0eeadbed";Ja.style=`
+`}}Ja.id="6d5b76fa99";Ja.style=`
   p {
     margin: 10px 0;
   }
@@ -660,7 +660,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
       </div>
     </div>
   </div>
-`}}_n.id="294d7d5756";function Kn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
+`}}_n.id="fbd150a5d6";function Kn(){return function(s){s.$;var e=s.$h;return s.$root,s.$f7,s.$f7route,s.$f7router,s.$theme,s.$update,s.$store,e`
 <div class="page">
   <div class="navbar">
     <div class="navbar-bg"></div>
@@ -681,7 +681,7 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
   </div>
 </div>
-`}}Kn.id="959d59793b";var Wp=[{path:"/",component:Xn},{path:"/about/",component:Gn},{path:"/form/",component:Un},{path:"/dynamic-route/blog/:blogId/post/:postId/",component:Ja},{path:"/request-and-load/user/:userId/",async:function({router:s,to:e,resolve:a}){var t=s.app;t.preloader.show(),e.params.userId,setTimeout(function(){var n={firstName:"Vladimir",lastName:"Kharlampidi",about:"Hello, i am creator of Framework7! Hope you like it!",links:[{title:"Framework7 Website",url:"http://framework7.io"},{title:"Framework7 Forum",url:"http://forum.framework7.io"}]};t.preloader.hide(),a({component:_n},{props:{user:n}})},1e3)}},{path:"(.*)",component:Kn}];Ot.use([wn]);Xa.use([un,pn,fn,hn,mn,En,Ua,Pn,Mn,An,In,On,Bn,Ln,Dn,Rn,Hn,zn]);const Xp=Da({state:{products:[{id:"1",title:"Apple iPhone 8",description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis."},{id:"2",title:"Apple iPhone 8 Plus",description:"Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!"},{id:"3",title:"Apple iPhone X",description:"Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum."}]},getters:{products({state:s}){return s.products}},actions:{addProduct({state:s},e){s.products=[...s.products,e]}}});function Qn(s,{$f7:e,$update:a}){let t="",n="";const r=l=>{t=l.target.value,a()},i=l=>{n=l.target.value,a()},o=()=>{e.dialog.alert("Username: "+t+"<br/>Password: "+n,()=>{e.loginScreen.close()})};return function(l){l.$;var c=l.$h;return l.$root,l.$f7,l.$f7route,l.$f7router,l.$theme,l.$update,l.$store,c`
+`}}Kn.id="f995f04b18";var Wp=[{path:"/",component:Xn},{path:"/about/",component:Gn},{path:"/form/",component:Un},{path:"/dynamic-route/blog/:blogId/post/:postId/",component:Ja},{path:"/request-and-load/user/:userId/",async:function({router:s,to:e,resolve:a}){var t=s.app;t.preloader.show(),e.params.userId,setTimeout(function(){var n={firstName:"Vladimir",lastName:"Kharlampidi",about:"Hello, i am creator of Framework7! Hope you like it!",links:[{title:"Framework7 Website",url:"http://framework7.io"},{title:"Framework7 Forum",url:"http://forum.framework7.io"}]};t.preloader.hide(),a({component:_n},{props:{user:n}})},1e3)}},{path:"(.*)",component:Kn}];Ot.use([wn]);Xa.use([un,pn,fn,hn,mn,En,Ua,Pn,Mn,An,In,On,Bn,Ln,Dn,Rn,Hn,zn]);const Xp=Da({state:{products:[{id:"1",title:"Apple iPhone 8",description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis."},{id:"2",title:"Apple iPhone 8 Plus",description:"Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!"},{id:"3",title:"Apple iPhone X",description:"Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum."}]},getters:{products({state:s}){return s.products}},actions:{addProduct({state:s},e){s.products=[...s.products,e]}}});function Qn(s,{$f7:e,$update:a}){let t="",n="";const r=l=>{t=l.target.value,a()},i=l=>{n=l.target.value,a()},o=()=>{e.dialog.alert("Username: "+t+"<br/>Password: "+n,()=>{e.loginScreen.close()})};return function(l){l.$;var c=l.$h;return l.$root,l.$f7,l.$f7route,l.$f7router,l.$theme,l.$update,l.$store,c`
   <div id="app">
 
     <!-- Left panel with cover effect-->
@@ -786,4 +786,4 @@ ${l.toString()}`)}return n&&(typeof n=="string"?o+=`?${n}`:Object.keys(n).length
     </div>
 
   </div>
-`}}Qn.id="76646aab16";new Xa({name:"framework7-v8-core-single-view",theme:"auto",el:"#app",component:Qn,store:Xp,routes:Wp});
+`}}Qn.id="a67c9e2a3c";new Xa({name:"framework7-v8-core-single-view",theme:"auto",el:"#app",component:Qn,store:Xp,routes:Wp});
